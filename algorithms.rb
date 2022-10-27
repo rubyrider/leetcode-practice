ae21f0423e0ffcb11ebeb1b5ca72a932a4c4635d
Irfan <irfandhk@gmail.com>
2022-10-27 19:32:21 +0200

    finish: merge sort

--- algorithms.rb
+++ algorithms.rb
@@ -5,4 +5,5 @@ Bundler.setup
 module Algorithms
   autoload :InsertionSort, './algorithms/insertion-sort'
   autoload :BinarySearch, './algorithms/binary-search'
+  autoload :MergeSort, './algorithms/merge-sort'
 end
--- algorithms/merge-sort.rb
+++ algorithms/merge-sort.rb
@@ -1,13 +1,39 @@
 module Algorithms
-  module InsertionSort
+  module MergeSort
     module_function
 
-    def main(array, start, mid, last)
-
+    def main(numbers)
+      merge_sort(numbers)
     end
 
-    def merge
+    def merge_sort(numbers)
+      length = numbers.size
 
+      return numbers if length == 1
+
+      mid = length/2
+
+      left_numbers = merge_sort(numbers[0..mid-1])
+      right_numbers = merge_sort(numbers[mid..length-1])
+
+      merge(left_numbers, right_numbers)
+    end
+
+    def merge(left_numbers, right_numbers)
+      sorted_numbers = []
+
+      while left_numbers[0] && right_numbers[0]
+        if left_numbers[0] < right_numbers[0]
+          sorted_numbers << left_numbers.shift
+        else
+          sorted_numbers << right_numbers.shift
+        end
+      end
+
+      sorted_numbers += left_numbers if left_numbers
+      sorted_numbers += right_numbers if right_numbers
+
+      sorted_numbers
     end
   end
 end
